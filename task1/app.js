988c80f1d6e00f401468311c70f1104ec62b9f85
Aychingva <aychinguliyeva@gmail.com>
2024-09-11 22:05:07 +0400

    add

---
+++ task1/app.js
@@ -0,0 +1,109 @@
+// let text="hello word";
+// console.log(text);
+
+//task1
+console.log("Menim adim Murad");
+
+//task2
+
+let name="Aychin";
+console.log(name);
+
+//task4
+
+let bookprice=750;
+let bookamount=14;
+console.log(bookprice*bookamount);
+
+//task5
+
+let kmspeed=75;
+let msspeed=kmspeed*1000/3600;
+console.log(msspeed);
+
+
+
+
+
+
+//task6
+// const age=prompt("yasinizi daxil edin:");
+// if(age%4==0){
+//     console.log((365+(age/4))*age);
+// }
+// else{
+//     const divide=(age/4)|0;
+//     console.log(divide);
+//     console.log((365+divide)*age);
+// }
+
+//task7
+
+const num=16;
+console.log(16**5);
+console.log(16%3);
+
+
+//task8
+
+
+let firstname="Alan";
+let lastname="Turing";
+console.log(`Sizin adiniz ${firstname} ${lastname}. \n Sizi gormeyime sadam!!`);
+
+
+//task10
+
+let name1="Aychin Guliyeva";
+console.log((name1.length)-1);
+
+
+//task11
+// let name2="Aychin Guliyeva";
+// console.log(name2[0],name2[7]);
+
+// //task12
+// const fullName = prompt("Tam adınızı 'AD SOYAD' formatında daxil edin:");
+
+
+// const spaceIndex = fullName.indexOf(" ");
+
+
+// const firstName = fullName.substring(0, spaceIndex);
+// const lastName = fullName.substring(spaceIndex + 1);
+    
+
+// console.log(`${lastName} ${firstName}`);
+
+
+//task13
+
+// const Name=prompt("adinizi daxil edin");
+// console.log(`${Name}`);
+
+//task14
+// const num1=+(prompt("ededi daxil edin"));
+// const num2=+(prompt("ededi daxil edin"));
+// const multiple=num1*num2;
+// const divide=num1/num2;
+// const substition=num1-num2;
+// const addition=num1+num2;
+// console.log(`${multiple}`)
+// console.log(`${divide}`)
+// console.log(`${substition}`)
+// console.log(`${addition}`)
+
+//task 15
+
+// const multiple=(12345*98765).toString();
+// const index=multiple[5];
+// console.log(multiple);
+// console.log(index);
+
+
+let a=10;
+let b=5;
+[a,b]=[b,a];
+console.log(a);
+console.log(b);
+
